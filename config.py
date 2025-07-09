7cfe0a7c1c01062818920bd2e2ed98409b977e6e
Egor-bit163 <datel1.iv@gmail.com>
2025-07-09 23:13:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "your_bot_token_here"
+BOT_TOKEN = "7549340742:AAHR77ckyohfS4rF5RSMu-zpqfAkHgXiol4"
 HONEYGAIN_REF = "https://r.honeygain.me/YOURCODE"
 FAUCETPAY_REF = "https://faucetpay.io/?r=YOURCODE"
 ROLLERCOIN_REF = "https://rollercoin.com/?r=YOURCODE"
