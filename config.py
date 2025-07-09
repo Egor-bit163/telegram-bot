37d553bad00e9a2c2d885351e8782e2381f17fec
Egor-bit163 <datel1.iv@gmail.com>
2025-07-09 23:22:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7549340742:AAHR77ckyohfS4rF5RSMu-zpqfAkHgXiol4"
-HONEYGAIN_REF = "https://r.honeygain.me/YOURCODE"
-FAUCETPAY_REF = "https://faucetpay.io/?r=YOURCODE"
-ROLLERCOIN_REF = "https://rollercoin.com/?r=YOURCODE"
+HONEYGAIN_REF = "https://r.honeygain.me/DATELCF308"
+FAUCETPAY_REF = "https://faucetpay.io/?r=9207641"
+
