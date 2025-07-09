3960ddce362ff1f58d7d01a608162fde29f57070
Egor-bit163 <datel1.iv@gmail.com>
2025-07-09 23:38:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-BOT_TOKEN = "7549340742:AAHR77ckyohfS4rF5RSMu-zpqfAkHgXiol4"
+BOT_TOKEN = os.getenv("7549340742:AAHR77ckyohfS4rF5RSMu-zpqfAkHgXiol4")
 HONEYGAIN_REF = "https://r.honeygain.me/DATELCF308"
 FAUCETPAY_REF = "https://faucetpay.io/?r=9207641"
 
